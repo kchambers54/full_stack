e295468a9909b50a07b0e27eae44c818c08770ba
kchambers54 <keller.chambers54@gmail.com>
2018-06-28 00:24:37 -0700

    making now front page

--- projects/2/courses/src/Components/SearchMethod.js
+++ projects/2/courses/src/Components/SearchMethod.js
@@ -3,7 +3,9 @@ import React from 'react'
 class SearchMethod extends React.Component{
     searchCourses(query) {
         console.log('SEARCHING COURSES: ' + query);
-        return fetch('https://www.eg.bucknell.edu/~amm042/service/q?' + query)
+        return fetch('https://www.eg.bucknell.edu/~amm042/service/q?' + query, {
+            credentials: 'include'
+        })
         .then((response) => response.json());
         
     }
