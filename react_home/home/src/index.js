7cf1ad2b7d6679c30175f18581cc7b99c80b4dfd
kchambers54 <keller.chambers54@gmail.com>
2018-06-30 12:11:13 -0700

    new stuff

---
+++ react_home/home/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+//Import bootstrap .css for reactstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
